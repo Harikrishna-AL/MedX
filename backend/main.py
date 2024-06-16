b8c4d8c373b2cb3435669d3cd6ec52f736cfef7a
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2024-06-17 00:43:27 +0530

    add: poisson blend endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from fastapi import Depends, FastAPI, HTTPException, status, Response
+from fastapi import Depends, FastAPI, HTTPException, status, Response, UploadFile, File
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
+from typing import List, Tuple, Optional
 from routes import auth
 import uvicorn
 import requests
@@ -12,6 +12,11 @@ from io import BytesIO
 import os
 import io
 import time
+from builtins import range
+import numpy as np
+import scipy.sparse
+import PIL.Image
+import pyamg
 
 COMFY_URI = "http://127.0.0.1:8188/"
 app = FastAPI()
@@ -32,6 +37,11 @@ class SAMDetect(BaseModel):
     negative_points: List[List[float]]
     threshold: float
 
+class Blend(BaseModel):
+    img_target: UploadFile = File(...)
+    img_source: UploadFile = File(...)
+    img_mask: UploadFile = File(...)
+    # offset: Tuple[int, int] = (0, 0)
 
 @app.post("/sam/detect")
 def detect(sam_detect: SAMDetect):
@@ -103,8 +113,101 @@ def get_output(workflow_json: dict, image_path: str):
     with open(os.path.join(COMFY_OUTPUT_LOCATION, latest_file), "rb") as f:
         return Response(f.read(), media_type="image/png")
     
+@app.post("/blend")
+def blend(data: Blend, offset = (0, 0)):
+    # compute regions to be blended
+    # img_target = Image.open(data.img_target.file)
+    # img_source = Image.open(data.img_source.file)
+    # img_mask = Image.open(data.img_mask.file)
+    # # offset = offset
 
+    # img_target = np.array(img_target)
+    # img_source = np.array(img_source)
+    # img_mask = np.array(img_mask)
 
+    # region_source = (
+    #     max(-offset[0], 0),
+    #     max(-offset[1], 0),
+    #     min(img_target.shape[0] - offset[0], img_source.shape[0]),
+    #     min(img_target.shape[1] - offset[1], img_source.shape[1]))
+    # region_target = (
+    #     max(offset[0], 0),
+    #     max(offset[1], 0),
+    #     min(img_target.shape[0], img_source.shape[0] + offset[0]),
+    #     min(img_target.shape[1], img_source.shape[1] + offset[1]))
+    # region_size = (region_source[2] - region_source[0], region_source[3] - region_source[1])
+
+    # # clip and normalize mask image
+    # img_mask = img_mask[region_source[0]:region_source[2], region_source[1]:region_source[3]]
+    # img_mask[img_mask == 0] = False
+    # img_mask[img_mask != False] = True
+
+    # # determines the diagonals on the coefficient matrix
+    # positions = np.where(img_mask)
+    # # setting the positions to be in a flatted manner
+    # positions = (positions[0] * region_size[1]) + positions[1]
+
+    # # row and col size of coefficient matrix
+    # n = np.prod(region_size)
+
+    # main_diagonal = np.ones(n)
+    # main_diagonal[positions] = 4
+    # diagonals = [main_diagonal]
+    # diagonals_positions = [0]
+
+    # # creating the diagonals of the coefficient matrix
+    # for diagonal_pos in [-1, 1, -region_size[1], region_size[1]]:
+    #     in_bounds_indices = None
+    #     if np.any(positions + diagonal_pos > n):
+    #         in_bounds_indices = np.where(positions + diagonal_pos < n)[0]
+    #     elif np.any(positions + diagonal_pos < 0):
+    #         in_bounds_indices = np.where(positions + diagonal_pos >= 0)[0]
+    #     in_bounds_positions = positions[in_bounds_indices]
+
+    #     diagonal = np.zeros(n)
+    #     diagonal[in_bounds_positions + diagonal_pos] = -1
+    #     diagonals.append(diagonal)
+    #     diagonals_positions.append(diagonal_pos)
+    # A = scipy.sparse.spdiags(diagonals, diagonals_positions, n, n, 'csr')
+
+    # # create poisson matrix for b
+    # P = pyamg.gallery.poisson(img_mask.shape)
+
+    # # get positions in mask that should be taken from the target
+    # inverted_img_mask = np.invert(img_mask.astype(np.bool)).flatten()
+    # positions_from_target = np.where(inverted_img_mask)[0]
+
+    # # for each layer (ex. RGB)
+    # for num_layer in range(img_target.shape[2]):
+    #     # get subimages
+    #     t = img_target[region_target[0]:region_target[2], region_target[1]:region_target[3], num_layer]
+    #     s = img_source[region_source[0]:region_source[2], region_source[1]:region_source[3], num_layer]
+    #     t = t.flatten()
+    #     s = s.flatten()
+
+    #     # create b
+    #     b = P * s
+    #     b[positions_from_target] = t[positions_from_target]
+
+    #     # solve Ax = b
+    #     x = scipy.sparse.linalg.spsolve(A, b)
+
+    #     # assign x to target image
+    #     x = np.reshape(x, region_size)
+    #     x = np.clip(x, 0, 255)
+    #     x = np.array(x, img_target.dtype)
+    #     img_target[region_target[0]:region_target[2], region_target[1]:region_target[3], num_layer] = x
+    
+    # img_target = Image.fromarray(img_target)
+    # # create a BytesIO object and save the image to it
+    # img_io = BytesIO()
+    # img_target.save(img_io, 'JPEG', quality=70)
+    # img_io.seek(0)
+    # # return a streaming response
+    # return StreamingResponse(img_io, media_type='image/jpeg')
+
+    # return img_target
+    return {"message": "success"}
 if __name__ == "__main__":
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
